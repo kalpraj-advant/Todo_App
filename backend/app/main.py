f678feb3557909d39983e014bf56ef89a804c79f
Rana Kalpraj <kalpraj@advant.xyz>
2025-06-10 14:03:19 +0530

    main

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["https://todo-app-snowy-mu-49.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
